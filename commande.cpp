6414a6392f542f899a519c99729181d344565f9d
bn612145 <bn612145@ad.u-bourgogne.fr@gr13-07l.(none)>
2023-01-18 15:43:57 +0100

    Mis a jour la classe Commande pour mieux convenir au panier client

--- commande.cpp
+++ commande.cpp
@@ -15,15 +15,42 @@ namespace mag{
 	}
 	std::string Commande::listeProduit(){
 		std::string text = "";
-		for(int i=0; i<_listeProduits.size(); i++){
+		/*for(int i=0; i<_listeProduits.size(); i++){
 			text += toString(_listeProduits.at(i));
+		}*/
+		for(int i=0; i<_titres.size(); i++){
+			text += _titres.at(i) + " " + _descriptions.at(i) + " " + std::to_string(_quantites.at(i)) + " " + std::to_string(_prix.at(i)) + "\n";
 		}
 		return text;
 	}
 
+	int Commande::searchEqTitre(std::string titre){
+		for (int i=0; i<_titres.size();i++){
+			if(_titres.at(i) == titre){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	void Commande::addProduit(std::string titre, std::string description, unsigned int quantite, double prix){
+		int a = searchEqTitre(titre);
+		if(a > 0){ //Mise a jour
+			_descriptions.at(a) = description;
+			_quantites.at(a) = quantite;
+			_prix.at(a) = prix;
+		}else{
+			_titres.push_back(titre);
+			_descriptions.push_back(description);
+			_quantites.push_back(quantite);
+			_prix.push_back(prix);
+		}
+	}
+
+
+
 	std::string toString(Commande c){
 		std::string text = c.getIdent() + "\n" + c.listeProduit();
-		//for qui prend les tostring des produits
 		return text;
 	}
 	std::ostream& operator<<(std::ostream& os, Commande& c){
--- commande.h
+++ commande.h
@@ -10,13 +10,19 @@
 namespace mag {
 	class Commande {
 	public:
-	Commande(std::string identClient = "anonymous");
-	std::string getIdent();
-	std::string listeProduit();
+		Commande(std::string identClient = "anonymous");
+		std::string getIdent();
+		std::string listeProduit();
+		void addProduit(std::string titre, std::string description, unsigned int quantite, double prix);
 	private:
+		int searchEqTitre(std::string titre); //Donne la position de titre doublon, pour mettre a jour un produit existant?
 		std::string _identClient;
-		std::vector<Produit> _listeProduits; //pas oublier que les produits sont copiés et que les changements ne se propageront pas
+		//std::vector<Produit> _listeProduits; //pas oublier que les produits sont copiés et que les changements ne se propageront pas
 		bool _status; //true = livrée
+		std::vector<std::string> _titres;
+		std::vector<std::string> _descriptions;
+		std::vector<unsigned int> _quantites;
+		std::vector<double> _prix;
 	};
 	std::string toString(Commande c);
 	std::ostream& operator<<(std::ostream& os, Commande& c);
